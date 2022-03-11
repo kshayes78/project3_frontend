b0efce864164d86108ca5926c6cfc29a42d321df
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-03-11 13:59:22 -0500

    styling completed

--- project3frontend/src/Components/PetCard.js
+++ project3frontend/src/Components/PetCard.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+// import { BsHeartFill, BsHeart } from "react-icons/bs";
+import { AiFillFire, AiOutlineFire } from "react-icons/ai";
 
 function PetCard({ name, url, species, breed, age, image, id, description }) {
+  const [favorites, setFavorites] = useState(false);
+
+  // const heartFillOrNot = favorites ? emptyHeart : filledHeart;
+
+  function changeHeart() {
+    setFavorites((favorites) => !favorites);
+  }
+
   return (
     <div className="petCard">
-      <h1 style={{color: "#5603ad"}}>{name}</h1>
+      <h1 style={{ color: "#5603ad" }}>{name}</h1>
+      {/* <BsSuitHeart onclick={changeHeart} /> */}
+      {favorites ? (
+        <button onClick={changeHeart} className="heart-emoji-active">
+          <AiFillFire style={{ fontSize: "20px", color: "coral" }} />
+        </button>
+      ) : (
+        <button onClick={changeHeart} className="heart-emoji-favorite">
+          <AiOutlineFire style={{ fontSize: "20px" }} />
+        </button>
+      )}
       <img src={image} alt="cute animal" />
       <br />
-      <p><strong>Species:</strong> {species}</p>
-      <p><strong>Breed:</strong> {breed}</p>
-      <p><strong>Age:</strong> {age}</p>
+      <p>
+        <strong>Species:</strong> {species}
+      </p>
+      <p>
+        <strong>Breed:</strong> {breed}
+      </p>
+      <p>
+        <strong>Age:</strong> {age}
+      </p>
       <p>{description}</p>
       <a href={url}>Take me to pet details</a>
     </div>
@@ -16,4 +42,3 @@ function PetCard({ name, url, species, breed, age, image, id, description }) {
 }
 
 export default PetCard;
- 
